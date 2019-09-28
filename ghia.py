ac8197ae847afeab7e6c32f851bfa8260c86526f
Lukas Kotlaba <lukas.kotlaba@gmail.com>
2019-09-28 10:57:52 +0200

    Implemented validation

--- ghia.py
+++ ghia.py
@@ -1,18 +1,60 @@
 import click
 import requests
 import configparser
+import re
 
 def validate_reposlug(ctx, param, reposlug):
-    pass
+    if re.match("[\\w-]+\\/[\\w-]+", reposlug):
+        return reposlug
+    else:
+        raise click.BadParameter("not in owner/repository format")
 
 def validate_auth(ctx, param, config_auth):
-    pass
+    try:
+        auth_parser = configparser.ConfigParser()
+        auth_parser.read(config_auth)
+        if "token" not in auth_parser["github"]:
+            raise click.BadParameter("incorrect configuration format")
+        return auth_parser["github"]["token"]
+    except KeyError:
+        raise click.BadParameter("incorrect configuration format")
 
 def validate_rules(ctx, param, config_rules):
-    pass
+    try:
+        rules_parser = configparser.ConfigParser()
+        rules_parser.optionxform = str
+        rules_parser.read(config_rules)
+        if "patterns" not in rules_parser:
+            raise click.BadParameter("incorrect configuration format")
+        rules = dict()
+
+        # Handle fallback
+        try:
+            fallback = rules_parser["fallback"]["label"]
+            if fallback is not None:
+                rules.update({"fallback":fallback})
+        except KeyError:
+            pass
+
+        # Handle patterns
+        for key, value in rules_parser["patterns"].items():
+            rules[key] = dict()
+            rules[key]["any"] = list()
+            rules[key]["title"] = list()
+            rules[key]["text"] = list()
+            rules[key]["label"] = list()
+            for line in value.split("\n"):
+                rule = line.split(":")
+                if len(rule) > 1:
+                    rules[key][rule[0]].append(":".join(rule[1:]))
+
+        return rules
+
+    except KeyError:
+        raise click.BadParameter("incorrect configuration format")
 
 @click.command()
-@click.argument("REPOSLUG", required=True, callback=validate_reposlug)
+@click.argument("reposlug", metavar="REPOSLUG", required=True, callback=validate_reposlug)
 @click.option("-s", "--strategy", help="How to handle assignment collisions.", type=click.Choice(["append", "set", "change"], case_sensitive=False), default="append", show_default=True)
 @click.option("-d", "--dry-run", help="Run without making any changes.", is_flag=True)
 @click.option("-a", "--config-auth", help="File with authorization configuration.", metavar="FILENAME", required=True, callback=validate_auth)
