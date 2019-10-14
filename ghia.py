8fdad65637413dc6841a38de3fe75579cf3c86b1
Lukas Kotlaba <lukas.kotlaba@gmail.com>
2019-10-14 22:56:19 +0200

    Improved HTML and code style

--- ghia.py
+++ ghia.py
@@ -4,13 +4,10 @@ import configparser
 import re
 import sys
 import os
-import json
 import flask
-import jinja2 
 import hmac
 
 GITHUB_URL = "https://api.github.com/repos/"
-WEB_REPOSLUG = "mi-pyt-ghia/kotlaluk-web"
 
 app = flask.Flask(__name__)
 
@@ -18,13 +15,22 @@ def read_config():
     config = dict()
     config["rules"] = dict()
     config["auth"] = dict()
+
+    # Read environment variables
+    try:
+        config["reposlug"] = os.environ["GHIA_REPOSLUG"]
+    except KeyError:
+        raise GhiaError("No GitHub repository specified")
+    
     try:
         config["auth"]["user"] = os.environ["GITHUB_USER"]
     except KeyError:
         raise GhiaError("No GitHub user specified")
+    
     try:
         config_files = os.environ["GHIA_CONFIG"].split(":")
         
+        # Process configuration files
         for file in config_files:
             errors = 0
             try:
@@ -53,6 +59,7 @@ def validate_signature(secret, header_signature, data):
 @app.route("/", methods=["GET", "POST"])
 def index():
     config = read_config()
+
     if flask.request.method == "GET":
         without_fallback = {login: config["rules"][login] \
                 for login in config["rules"] if login != "fallback"}
@@ -62,34 +69,35 @@ def index():
             fallback = None
         return flask.render_template("index.html", user=config["auth"]["user"], \
         rules=without_fallback, fallback=fallback)
+
     if flask.request.method == "POST":
+        # Perform validation if present
         try:
             signature = flask.request.headers["X-Hub-Signature"]
             if not validate_signature(str(config["auth"]["secret"]), signature, flask.request.data):
                 flask.abort(401)
         except KeyError:
             pass
+
+        # Process received action
         try:
             event = flask.request.headers["X-GitHub-Event"]
             if event == "ping":
+                app.logger.info("Received PING event")
                 return "Ping successful"
             elif event =="issues":
                 if flask.request.json["action"] in ("opened", "edited", "transferred", \
                     "reopened", "assigned", "unassigned", "labeled", "unlabeled"):
+                    app.logger.info("Received ISSUES event")
                     payload = flask.request.json["isssue"]
-                    number = payload["number"]
-                    url = payload["url"]
-                    html_url = payload["html_url"]
-                    title = payload["title"]
-                    body = payload["body"]
-                    labels = [x["name"] for x in payload["labels"]]
-                    assignees = [x["login"] for x in payload["assignees"]]
-                    issue = Issue(number, url, html_url, title, body, labels, assignees)
+                    issue = create_issue(payload)
+
                     session = requests.Session()
                     session.headers["Authorization"] = f"token {config['auth']['token']}"
                     session.headers["Accept"] = "application/vnd.github.v3+json"
-                    process_issue(issue, WEB_REPOSLUG, "append", config["rules"], False, session, print_output=False)
-                    return "The issue was successfully updated."
+                    process_issue(issue, config["reposlug"], "append", config["rules"], False, session, print_output=False)
+                    app.logger.info("Issue sent successfully")
+                    return f"Issue {issue.number} ({issue.title}) was successfully updated."
                 else:
                     flask.abort(501)
             else:
@@ -123,6 +131,16 @@ class Issue:
         if self.update_assignees:
             output["assignees"] = list(self.assignees)
         return output
+
+def create_issue(payload):
+    number = payload["number"]
+    url = payload["url"]
+    html_url = payload["html_url"]
+    title = payload["title"]
+    body = payload["body"]
+    labels = [x["name"] for x in payload["labels"]]
+    assignees = [x["login"] for x in payload["assignees"]]
+    return Issue(number, url, html_url, title, body, labels, assignees)
 
 def validate_reposlug(ctx, param, reposlug):
     if re.match("[\\w-]+\\/[\\w-]+", reposlug):
@@ -278,14 +296,7 @@ def process_repository(reposlug, strategy, token, rules, dry_run):
             
             # Extract issues from the page
             for i in response.json():
-                number = i["number"]
-                url = i["url"]
-                html_url = i["html_url"]
-                title = i["title"]
-                body = i["body"]
-                labels = [x["name"] for x in i["labels"]]
-                assignees = [x["login"] for x in i["assignees"]]
-                issue = Issue(number, url, html_url, title, body, labels, assignees)
+                issue = create_issue(i)
 
                 # Process the issue
                 try:
