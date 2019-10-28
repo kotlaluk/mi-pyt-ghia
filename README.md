e85d235dfb7c01569a264a6baddb976762cf57d7
Lukas Kotlaba <lukas.kotlaba@gmail.com>
2019-10-28 15:22:35 +0100

    Preparation for packaging

---
+++ README.md
@@ -0,0 +1,6 @@
+# GHIA - GitHub Issue Assigner
+
+## About
+
+GHIA is a command-line and web application for automatic assignment of GitHub 
+issues based on their content. It utilizes GitHub API and supports GitHub webhooks.
---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = ''.join(f.readlines())
+
+setup(
+    name='ghia_kotlaluk',
+    version='0.1',
+    description='GitHub Issue Assigner',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Lukáš Kotlaba',
+    author_email='lukas.kotlaba@gmail.com',
+    keywords='ghia,github',
+    license='GNU GPLv3',
+    url='https://github.com/kotlaluk/mi-pyt-ghia',
+    packages=find_packages(),
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Version Control',
+        'Topic :: Software Development :: Version Control :: Git'
+        ],
+    install_requires=['Flask', 'click', 'requests'],
+    entry_points={
+        'console_scripts': [
+            'ghia = ghia.cli:cli',
+        ],
+    },
+    package_data={'ghia': ['templates/*.html', 'static/*.css']},
+    zip_safe=False,
+)
