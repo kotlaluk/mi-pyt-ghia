20ef9e820d58730820b904f501377b9ef276595d
Lukas Kotlaba <lukas.kotlaba@gmail.com>
2019-12-02 22:35:02 +0100

    Updated version to 0.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.rst') as f:
 
 setup(
     name='ghia_kotlaluk',
-    version='0.4.1',
+    version='0.4.2',
     description='GitHub Issue Assigner',
     long_description=long_description,
     long_description_content_type="text/markdown",
