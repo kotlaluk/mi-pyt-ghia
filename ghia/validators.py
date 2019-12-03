3503dd67824d8b47f8159cf2531dcf0f07d92a33
Lukas Kotlaba <lukas.kotlaba@gmail.com>
2019-12-03 23:37:13 +0100

    Docs for validators

--- ghia/validators.py
+++ ghia/validators.py
@@ -1,3 +1,11 @@
+"""
+.. module:: validators
+
+This module contains validation functions that are used as callbacks for
+validating user input from CLI app, validate contents of configuration files and
+received payload messages.
+"""
+
 import re
 import hmac
 import configparser
@@ -5,6 +13,23 @@ from click import BadParameter
 
 
 def validate_reposlug(ctx, param, reposlug):
+    """Callback to validate the provided reposlug.
+
+    The function checks whether the provided value for *reposlug* is in owner/
+    repository format.
+
+    Args:
+        ctx: context object obtained from click
+        param: parameter object obtained from click
+        reposlug (str): GitHub reposlug in owner/repository format
+
+    Raises:
+        BadParameter: if the provided *reposlug* is not in owner/repository format
+
+    Returns:
+        str: the provided *reposlug*, if valid
+    """
+
     if re.match("^[\\w-]+\\/[\\w-]+$", reposlug):
         return reposlug
     else:
@@ -12,6 +37,24 @@ def validate_reposlug(ctx, param, reposlug):
 
 
 def validate_auth(ctx, param, config_auth):
+    """Callback to validate the provided authentication file.
+
+    The function verifies the provided authentication file, especially the
+    presence of "token" key in the "github" section.
+
+    Args:
+        ctx: context object obtained from click
+        param: parameter object obtained from click
+        config_auth (str): name of the file containing configuraion for
+                           authentication
+
+    Raises:
+        BadParameter: if the provided file has incorrect configuration format
+
+    Returns:
+        dict: contents of the "github" section from the provided file
+    """
+
     try:
         auth_parser = configparser.ConfigParser()
         auth_parser.read(config_auth)
@@ -23,6 +66,29 @@ def validate_auth(ctx, param, config_auth):
 
 
 def validate_rules(ctx, param, config_rules):
+    """Callback to validate the provided file with rules.
+
+    The rules are loaded from the file provided in *config_rules* parameter and
+    processed to a nested dictionary. In the first level, the keys of the
+    dictionary are the labels used in the rules and "fallback" key (if present
+    in the rules). The second level keys are "title", "text", "label" or "any"
+    (applicable for non-fallback rules). For these keys, the value is a list of
+    compiled regular expression objects. These objects should be used to match
+    the respective part of a GitHub issue.
+
+    Args:
+        ctx: context object obtained from click
+        param: parameter object obtained from click
+        config_rules (str): name of the file containing the rules for issue
+                            assignment
+
+    Raises:
+        BadParameter: if the provided file has incorrect configuration format
+
+    Returns:
+        dict: nested dictionary containing loaded issue assignment rules.
+    """
+
     try:
         rules_parser = configparser.ConfigParser()
         rules_parser.optionxform = str
@@ -58,6 +124,23 @@ def validate_rules(ctx, param, config_rules):
 
 
 def validate_signature(secret, data, header_signature):
+    """Validate GitHub signature.
+
+    Validates payloads received in requests from GitHub. GitHub uses an HMAC
+    hexdigest to compute the hash of a payload and sets the result in the
+    X-Hub-Signature header. This function computes the HMAC hexdigest from
+    *data* and *secret* and compares it to *header_signature*.
+
+    Args:
+        secret (str): secret token set up with GitHub to secure messages
+        data (str): payload from the request
+        header_signature (str): value obtained from the X-Hub-Signature GitHub
+                                header
+
+    Returns:
+        bool: True if the values match, False otherwise
+    """
+
     digest_mode, signature = header_signature.split("=")
     mac = hmac.new(bytes(secret, "ascii"), msg=data,
                    digestmod=digest_mode)
